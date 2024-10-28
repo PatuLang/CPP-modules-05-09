d8e3c34954940dd0be3486f9385bbc991df9899a
PatuLang <patrik.laang@gmail.com>
2024-10-28 18:49:00 +0200

    main and makefile now has the intern implemented

--- module05/ex03/main.cpp
+++ module05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: plang <plang@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 10:05:40 by plang             #+#    #+#             */
-/*   Updated: 2024/10/28 15:01:30 by plang            ###   ########.fr       */
+/*   Updated: 2024/10/28 18:43:26 by plang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,33 @@
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
+#include "Intern.hpp"
 
 
 int	main(void)
 {
 	try
 	{
+		Intern randomIntern;
+		AForm* rrf;
+		rrf = randomIntern.makeForm("Robotomy request", "Eemil");
 		Bureaucrat Bob("Bob", 1);
 		Bureaucrat Tom("Tom", 2);
 		Bureaucrat Dom(Tom);
 		std::cout << Dom << '\n';
 		PresidentialPardonForm one("Sauli");
-		RobotomyRequestForm two("Rob_robot");
+		// RobotomyRequestForm two("Rob_robot");
 		ShrubberyCreationForm three("City_park");
 		std::cout << one << "\n";
-		std::cout << two << "\n";
+		std::cout << rrf << "\n";
 		std::cout << three << "\n";
 		Tom.signForm(one);
 		Bob.executeForm(one);
-		Tom.signForm(two);
-		Bob.executeForm(two);
+		Tom.signForm(*rrf);
+		Bob.executeForm(*rrf);
 		Tom.signForm(three);
 		Bob.executeForm(three);
+		delete rrf;
 	}
 	catch(const std::exception& e)
 	{
