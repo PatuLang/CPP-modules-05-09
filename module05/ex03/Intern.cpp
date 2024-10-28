574c225febd9b1a1884953afbb727055ed9e84be
PatuLang <patrik.laang@gmail.com>
2024-10-28 18:48:21 +0200

    Intern with a switch to select the right Form to create and its own exception

---
+++ module05/ex03/Intern.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Intern.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plang <plang@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 15:18:05 by plang             #+#    #+#             */
+/*   Updated: 2024/10/28 18:41:59 by plang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Intern.hpp"
+
+Intern::Intern()
+{
+}
+
+Intern::Intern(const Intern&)
+{
+}
+
+Intern& Intern::operator=(const Intern&)
+{
+	return *this;
+}
+
+
+Intern::~Intern()
+{
+}
+
+AForm*	Intern::makeForm(std::string formName, std::string formTarget)
+{
+	AForm *newForm;
+	std::string _forms[3] = {{"Robotomy request"}, {"Shrubbery creation"}, {"Presidential pardon"}};
+	for(int i = 0; i < 3; i++)
+	{
+		if (formName == _forms[i])
+		{
+			switch (i)
+			{
+				case 0:
+				{
+					newForm = new RobotomyRequestForm(formTarget);
+					std::cout << "Intern creates " << formName << "\n";
+					break;
+				}
+				case 1:
+				{
+					newForm = new ShrubberyCreationForm(formTarget);
+					std::cout << "Intern creates " << formName << "\n";
+					break;
+				}
+				case 2:
+				{
+					newForm = new PresidentialPardonForm(formTarget);
+					std::cout << "Intern creates " << formName << "\n";
+					break;
+				}
+			}
+			return newForm;
+		}
+	}
+	throw FormDoesNotExistException();
+}
+
+const char* Intern::FormDoesNotExistException::what() const noexcept
+{
+	return "The requested form does not exist!";
+}
---
+++ module05/ex03/Intern.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Intern.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plang <plang@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 15:18:21 by plang             #+#    #+#             */
+/*   Updated: 2024/10/28 17:28:52 by plang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INTERN_HPP
+# define INTERN_HPP
+
+# include <iostream>
+# include "PresidentialPardonForm.hpp"
+# include "ShrubberyCreationForm.hpp"
+# include "RobotomyRequestForm.hpp"
+
+class Form;
+
+class Intern
+{
+	private:
+
+	public:
+		Intern();
+		Intern(const Intern&);
+		Intern&	operator=(const Intern&);
+		~Intern();
+		
+		AForm*	makeForm(std::string, std::string);
+
+	class FormDoesNotExistException : public std::exception
+	{
+		public:
+			const char* what() const noexcept;
+	};
+};
+
+#endif
