bd80ac6e1687f9511b741f408948d4d0c87e9f51
PatuLang <patrik.laang@gmail.com>
2024-10-29 12:37:07 +0200

    initial commit to module06 ex00

---
+++ module06/ex00/ScalarConverter.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plang <plang@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 12:23:53 by plang             #+#    #+#             */
+/*   Updated: 2024/10/29 12:26:08 by plang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+# include <iostream>
+
+class ScalarConterter
+{
+	private:
+
+	public:
+
+};
+
+#endif
