c3ea08df73e66bcb413292f16e6a8466eb4c0f1e
PatuLang <patrik.laang@gmail.com>
2024-11-12 17:26:32 +0200

    main clean up and variable names changed to more suitable onnes

--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: plang <plang@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 16:59:15 by plang             #+#    #+#             */
-/*   Updated: 2024/11/11 11:51:25 by plang            ###   ########.fr       */
+/*   Updated: 2024/11/12 17:20:35 by plang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,33 +16,33 @@ int	main(void)
 {
 	try
 	{
-		Array<std::string> two(6);
+		Array<std::string> xArrObj(6);
 		std::string word[] = {"Hello", "from", "the", "other", "side"};
 
-		for (unsigned int i = 0; i < std::min(static_cast<unsigned long>(two.size()), sizeof(word)/sizeof(word[0])); i++)
-			two[i] = word[i];
-		for (unsigned int i = 0; i < two.size(); i++)
-			std::cout << two[i] << "\n";
+		for (unsigned int i = 0; i < std::min(static_cast<unsigned long>(xArrObj.size()), sizeof(word)/sizeof(word[0])); i++)
+			xArrObj[i] = word[i];
+		for (unsigned int i = 0; i < xArrObj.size(); i++)
+			std::cout << xArrObj[i] << "\n";
 		std::cout << "\n";
 
-		Array<std::string> four;
-		four = two;
+		Array<std::string> yArrObj;
+		yArrObj = xArrObj;
 		std::string morewords[] = {"What", "is", "your", "name"};
 
-		for (unsigned int i = 0; i < std::min(static_cast<unsigned long>(four.size()), sizeof(morewords)/sizeof(morewords[0])); i++)
-			four[i] = morewords[i];
-		for (unsigned int i = 0; i < four.size(); i++)
-			std::cout << four[i] << "\n";
+		for (unsigned int i = 0; i < std::min(static_cast<unsigned long>(yArrObj.size()), sizeof(morewords)/sizeof(morewords[0])); i++)
+			yArrObj[i] = morewords[i];
+		for (unsigned int i = 0; i < yArrObj.size(); i++)
+			std::cout << yArrObj[i] << "\n";
 
-		two[4] = "world";
+		xArrObj[4] = "world";
 		std::cout << "\n";
 
-		for (unsigned int i = 0; i < two.size(); i++)
-			std::cout << two[i] << "\n";
+		for (unsigned int i = 0; i < xArrObj.size(); i++)
+			std::cout << xArrObj[i] << "\n";
 		std::cout << "\n";
 
-		for (unsigned int i = 0; i < four.size(); i++)
-			std::cout << four[i] << "\n";
+		for (unsigned int i = 0; i < yArrObj.size(); i++)
+			std::cout << yArrObj[i] << "\n";
 		std::cout << "\n";
 	}
 	catch(const std::exception& e)
@@ -51,33 +51,33 @@ int	main(void)
 	}
 	try
 	{
-		Array<const char *> two(5);
+		Array<const char *> xArrObj(5);
 		const char * word[] = {"Hello", "from", "the", "other", "side"};
 
-		for (unsigned int i = 0; i < std::min(static_cast<unsigned long>(two.size()), sizeof(word)/sizeof(word[0])); i++)
-			two[i] = word[i];
-		for (unsigned int i = 0; i < two.size(); i++)
-			std::cout << two[i] << "\n";
+		for (unsigned int i = 0; i < std::min(static_cast<unsigned long>(xArrObj.size()), sizeof(word)/sizeof(word[0])); i++)
+			xArrObj[i] = word[i];
+		for (unsigned int i = 0; i < xArrObj.size(); i++)
+			std::cout << xArrObj[i] << "\n";
 		std::cout << "\n";
 
-		Array<const char *> four;
-		four = two;
+		Array<const char *> yArrObj;
+		yArrObj = xArrObj;
 		const char * morewords[] = {"What", "is", "your", "name"};
 
-		for (unsigned int i = 0; i < std::min(static_cast<unsigned long>(four.size()), sizeof(morewords)/sizeof(morewords[0])); i++)
-			four[i] = morewords[i];
-		for (unsigned int i = 0; i < four.size(); i++)
-			std::cout << four[i] << "\n";
+		for (unsigned int i = 0; i < std::min(static_cast<unsigned long>(yArrObj.size()), sizeof(morewords)/sizeof(morewords[0])); i++)
+			yArrObj[i] = morewords[i];
+		for (unsigned int i = 0; i < yArrObj.size(); i++)
+			std::cout << yArrObj[i] << "\n";
 
-		two[4] = "world";
+		xArrObj[4] = "world";
 		std::cout << "\n";
 
-		for (unsigned int i = 0; i < two.size(); i++)
-			std::cout << two[i] << "\n";
+		for (unsigned int i = 0; i < xArrObj.size(); i++)
+			std::cout << xArrObj[i] << "\n";
 		std::cout << "\n";
 
-		for (unsigned int i = 0; i < four.size(); i++)
-			std::cout << four[i] << "\n";
+		for (unsigned int i = 0; i < yArrObj.size(); i++)
+			std::cout << yArrObj[i] << "\n";
 		std::cout << "\n";
 	}
 	catch(const std::exception& e)
@@ -86,32 +86,32 @@ int	main(void)
 	}
 	try
 	{
-		Array<int> two(7);
+		Array<int> xArrObj(7);
 		int numbers[] = {1, 2, 2147483647, 4, 5, 6, 7, 8, 40};
 
-		for (unsigned int i = 0; i < std::min(static_cast<unsigned long>(two.size()), sizeof(numbers)/sizeof(numbers[0])); i++)
-			two[i] = numbers[i];
-		for (unsigned int i = 0; i < two.size(); i++)
-			std::cout << two[i] << "\n";
+		for (unsigned int i = 0; i < std::min(static_cast<unsigned long>(xArrObj.size()), sizeof(numbers)/sizeof(numbers[0])); i++)
+			xArrObj[i] = numbers[i];
+		for (unsigned int i = 0; i < xArrObj.size(); i++)
+			std::cout << xArrObj[i] << "\n";
 		std::cout << "\n";
 
-		Array<int> four(two);
+		Array<int> yArrObj(xArrObj);
 		int morenumbers[] = {100, 200, 300, 400, 500, 500, 600};
 
-		for (unsigned int i = 0; i < std::min(static_cast<unsigned long>(four.size()), sizeof(morenumbers)/sizeof(morenumbers[0])); i++)
-			four[i] = morenumbers[i];
-		for (unsigned int i = 0; i < four.size(); i++)
-			std::cout << four[i] << "\n";
+		for (unsigned int i = 0; i < std::min(static_cast<unsigned long>(yArrObj.size()), sizeof(morenumbers)/sizeof(morenumbers[0])); i++)
+			yArrObj[i] = morenumbers[i];
+		for (unsigned int i = 0; i < yArrObj.size(); i++)
+			std::cout << yArrObj[i] << "\n";
 
-		two[4] = 10;
+		xArrObj[4] = 10;
 		std::cout << "\n";
 
-		for (unsigned int i = 0; i < two.size(); i++)
-			std::cout << two[i] << "\n";
+		for (unsigned int i = 0; i < xArrObj.size(); i++)
+			std::cout << xArrObj[i] << "\n";
 		std::cout << "\n";
 
-		for (unsigned int i = 0; i < four.size(); i++)
-			std::cout << four[i] << "\n";
+		for (unsigned int i = 0; i < yArrObj.size(); i++)
+			std::cout << yArrObj[i] << "\n";
 		std::cout << "\n";
 	}
 	catch(const std::exception& e)
@@ -120,32 +120,32 @@ int	main(void)
 	}
 	try
 	{
-		Array<double> two(10);
+		Array<double> xArrObj(10);
 		double numbers[] = {1.9, 2.9, 3.9, 4.9, 5.9, 6.9, 7.9};
 
-		for (unsigned int i = 0; i < std::min(static_cast<unsigned long>(two.size()), sizeof(numbers)/sizeof(numbers[0])); i++)
-			two[i] = numbers[i];
-		for (unsigned int i = 0; i < two.size(); i++)
-			std::cout << two[i] << "\n";
+		for (unsigned int i = 0; i < std::min(static_cast<unsigned long>(xArrObj.size()), sizeof(numbers)/sizeof(numbers[0])); i++)
+			xArrObj[i] = numbers[i];
+		for (unsigned int i = 0; i < xArrObj.size(); i++)
+			std::cout << xArrObj[i] << "\n";
 		std::cout << "\n";
 
-		Array<double> four(two);
+		Array<double> yArrObj(xArrObj);
 		double morenumbers[] = {10.9, 20.9, 30.9, 40.9, 50.9, 60.9, 70.9};
 
-		for (unsigned int i = 0; i < std::min(static_cast<unsigned long>(four.size()), sizeof(morenumbers)/sizeof(morenumbers[0])); i++)
-			four[i] = morenumbers[i];
-		for (unsigned int i = 0; i < four.size(); i++)
-			std::cout << four[i] << "\n";
+		for (unsigned int i = 0; i < std::min(static_cast<unsigned long>(yArrObj.size()), sizeof(morenumbers)/sizeof(morenumbers[0])); i++)
+			yArrObj[i] = morenumbers[i];
+		for (unsigned int i = 0; i < yArrObj.size(); i++)
+			std::cout << yArrObj[i] << "\n";
 
-		two[4] = 10.95959;
+		xArrObj[4] = 10.95959;
 		std::cout << "\n";
 
-		for (unsigned int i = 0; i < two.size(); i++)
-			std::cout << two[i] << "\n";
+		for (unsigned int i = 0; i < xArrObj.size(); i++)
+			std::cout << xArrObj[i] << "\n";
 		std::cout << "\n";
 
-		for (unsigned int i = 0; i < four.size(); i++)
-			std::cout << four[i] << "\n";
+		for (unsigned int i = 0; i < yArrObj.size(); i++)
+			std::cout << yArrObj[i] << "\n";
 	}
 	catch(const std::exception& e)
 	{
@@ -153,54 +153,3 @@ int	main(void)
 	}
 	return 0;
 }
-
-// #define MAX_VAL 750
-// int main(int, char**)
-// {
-//     Array<int> numbers(MAX_VAL);
-//     int* mirror = new int[MAX_VAL];
-//     srand(time(NULL));
-//     for (int i = 0; i < MAX_VAL; i++)
-//     {
-//         const int value = rand();
-//         numbers[i] = value;
-//         mirror[i] = value;
-//     }
-//     //SCOPE
-//     {
-//         Array<int> tmp = numbers;
-//         Array<int> test(tmp);
-//     }
-
-//     for (int i = 0; i < MAX_VAL; i++)
-//     {
-//         if (mirror[i] != numbers[i])
-//         {
-//             std::cerr << "didn't save the same value!!" << std::endl;
-//             return 1;
-//         }
-//     }
-//     try
-//     {
-//         numbers[2] = 0;
-//     }
-//     catch(const std::exception& e)
-//     {
-//         std::cerr << e.what() << '\n';
-//     }
-//     try
-//     {
-//         numbers[MAX_VAL] = 0;
-//     }
-//     catch(const std::exception& e)
-//     {
-//         std::cerr << e.what() << '\n';
-//     }
-
-//     for (int i = 0; i < MAX_VAL; i++)
-//     {
-//         numbers[i] = rand();
-//     }
-//     delete [] mirror;//
-//     return 0;
-// }
