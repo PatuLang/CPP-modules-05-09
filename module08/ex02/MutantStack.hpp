1ddbe61364f0f4c7cca8032a4fcb39a7de947e30
PatuLang <patrik.laang@gmail.com>
2024-11-14 12:58:50 +0200

    initial commit for ex02

---
+++ module08/ex02/MutantStack.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plang <plang@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 12:55:33 by plang             #+#    #+#             */
+/*   Updated: 2024/11/14 12:57:30 by plang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <algorithm>
+# include <limits>
+
+
+
+
+# include "MutantStack.tpp"
+
+#endif
